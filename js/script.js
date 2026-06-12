779bb32646a4d06fabcfb8615326b80aea895b41
farouk24967 <bouayedfarouk63@gmail.com>
2026-06-12 14:15:35 +0100

    vcv

--- js/script.js
+++ js/script.js
@@ -169,6 +169,8 @@ function initLogoLoop(containerId) {
     clients.forEach(name => {
       const li = document.createElement('li');
       li.className = 'logoloop__item';
+      if (name.includes('LOGO_ASSA')) li.classList.add('invert-logo');
+      if (name.includes('qaada')) li.classList.add('invert-logo');
       li.innerHTML = '<div class="logoloop__logo"><img src="public/images/' + name + '" alt="" class="logoloop__img"></div>';
       ul.appendChild(li);
     });
